57e6be27d301690fe03d3d83550c0efed0c9f4f5
Roberto Gadiel Vides Pérez <gadielvides@gmail.com>
2022-03-16 14:12:49 -0600

    Modificación de Ejercicio

--- Ejercicio1/src/Ejercicio1/Main.java
+++ Ejercicio1/src/Ejercicio1/Main.java
@@ -3,20 +3,26 @@ package Ejercicio1;
 public class Main {
 
     public static void main(String[] args) {
-      
+
         Persona persona1 = new Persona();
         persona1.NomSexEdPeAl();
         persona1.esMayorDeEdad();
         persona1.CalcularIMC();
-        persona1.comprobarSexo();
-        persona1.to_String();
+        persona1.comprobarSexo('h');
+        persona1.Informacion();
         persona1.generaDUI();
 
         Persona persona2 = new Persona();
         persona2.NomSexEd();
         persona2.esMayorDeEdad();
-        persona2.comprobarSexo();
-        persona2.to_String();
+        persona2.Informacion();
+
+        Persona persona3 = new Persona();
+        persona3.setNombre("Carlos");
+        persona3.setEdad(18);
+        persona3.setSexo('h');
+        persona3.setPesoYAltura(45, 1.72);
+        persona3.Informacion();
     }
 
 }
--- Ejercicio1/src/Ejercicio1/Persona.java
+++ Ejercicio1/src/Ejercicio1/Persona.java
@@ -7,16 +7,56 @@ public class Persona {
 
     private final JTextArea informacion = new JTextArea();
     private String salida = "Información de la Persona\n";
-    private String Nombre, sexo = "Hombre";
+    private String Nombre;
     private int edad;
     private double peso, altura;
+    private char sexo;
 
-    public Persona() {
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+        salida = salida + "Nombre: \t" + this.Nombre + "\n";
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+        salida = salida + "Edad: \t" + this.edad + "\n";
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+        if (this.sexo == 'h') {
+            this.sexo = 'H';
+        } else if (this.sexo == 'm') {
+            this.sexo = 'M';
+        } else {
+            this.sexo = 'H';
+        }
+        salida = salida + "Sexo: \t" + this.sexo + "\n";
+    }
+
+    public void setPesoYAltura(double peso, double altura) {
+        this.peso = peso;
+        this.altura = altura;
+        String funcion = " ";
+
+        int calculo = (int) (this.peso / Math.pow(this.altura, 2));
+        if (calculo < 20) {
+            funcion = "-1 Por debajo del peso ideal";
+
+        } else if (calculo >= 20 && calculo <= 25) {
+            funcion = "0 Peso ideal";
+
+        } else if (calculo > 25) {
+            funcion = "1 Sobrepeso";
+
+        }
+        salida = salida + "Peso ideal: \t" + funcion + "\n";
+
     }
 
     public void NomSexEdPeAl() {
         Nombre = JOptionPane.showInputDialog("Ingrese el nombre de la persona");
-        sexo = JOptionPane.showInputDialog("Ingrese el sexo: \nH: Hombre M: Mujer");
+
         edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad"));
         peso = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el peso de la persona"));
         altura = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la altura de la persona"));
@@ -25,7 +65,7 @@ public class Persona {
 
     public void NomSexEd() {
         Nombre = JOptionPane.showInputDialog("Ingrese el nombre de la persona");
-        sexo = JOptionPane.showInputDialog("Ingrese el sexo: \nH: Hombre M: Mujer");
+
         edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad"));
 
         salida = salida + "Nombre: \t" + Nombre + "\n";
@@ -54,20 +94,18 @@ public class Persona {
         salida = salida + "Mayor de Edad: \t" + mayorEdad + "\n";
     }
 
-    public void comprobarSexo() {
-   
-        if (sexo.equalsIgnoreCase("h")) {
-            sexo = "H";
-        } else if (sexo.equalsIgnoreCase("m")) {
-            sexo = "M";
+    public void comprobarSexo(char sexo) {
+        if (sexo == 'h') {
+            sexo = 'H';
+        } else if (sexo == 'm') {
+            sexo = 'M';
         } else {
-            sexo = "H";
+            sexo = 'H';
         }
         salida = salida + "Sexo: \t" + sexo + "\n";
-
     }
 
-    public void to_String() {
+    public void Informacion() {
         informacion.setText(salida);
         JOptionPane.showMessageDialog(null, informacion);
     }
